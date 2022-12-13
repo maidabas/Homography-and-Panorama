5cd95194c770cd14cf8cd310446c3e50bb467754
adigreen26 <117349919+adigreen26@users.noreply.github.com>
2022-12-13 12:49:19 +0200

    compute_forward_homography_fast
    Almost done - left to see pic in color

--- ex1_student_solution.py
+++ ex1_student_solution.py
@@ -110,8 +110,32 @@ class Solution:
             The forward homography of the source image to its destination.
         """
         # return new_image
-        """INSERT YOUR CODE HERE"""
-        pass
+        target_image = np.zeros(shape=dst_image_shape)
+
+        # (1) Create a meshgrid of columns and rows.
+        H = dst_image_shape[0]
+        W = dst_image_shape[1]
+        yv, xv = np.meshgrid(np.arange(W), np.arange(H), indexing='ij')
+        # (2) Generate a matrix of size 3x(H*W) which stores the pixel locations in homogeneous coordinates.
+        coor_matrix = np.ones((3, H*W), dtype=int)
+        coor_matrix[0, :] = yv.flatten()
+        coor_matrix[1, :] = xv.flatten()
+        # (3) Transform the source homogeneous coordinates to the target homogeneous coordinates
+        new_coor = np.matmul(homography, coor_matrix)
+        # (4) normalization + round + clip
+        new_coor = np.round(new_coor / new_coor[2, :]).astype(int)
+        temp_row0 = new_coor[0, :]
+        temp_row1 = new_coor[1, :]
+        cond1 = np.logical_and(temp_row0 < W,  temp_row0 >= 0)
+        cond2 = np.logical_and(temp_row1 < H,  temp_row1 >= 0)
+        cond = np.logical_and(cond1, cond2)
+        # (5) Plant the pixels from the source image to the target image
+        src_row0 = coor_matrix[0, :]
+        src_row1 = coor_matrix[1, :]
+        src_row0[cond] = 0
+        src_row1[cond] = 0
+        target_image[temp_row1[cond == True], temp_row0[cond == True]] = src_image[src_row1[cond == True], src_row0[cond == True]] / 255.0
+        return target_image
 
     @staticmethod
     def test_homography(homography: np.ndarray,
